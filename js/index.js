b2966a7668d7d2f8e658e16313379b72d74a8ad6
tamagoru <marushannnn@outlook.com>
2023-04-26 18:41:42 -0300

    slideshow finished

---
+++ js/index.js
@@ -0,0 +1,52 @@
+var timeOut = 2000;
+var slideIndex = 0;
+var autoOn = true;
+
+autoSlides();
+
+function autoSlides() {
+    timeOut = timeOut - 20;
+
+    if (autoOn == true && timeOut < 0) {
+        showSlides();
+    }
+    setTimeout(autoSlides, 20);
+}
+
+function prevSlide() {
+
+    timeOut = 2000;
+
+    var slides = document.getElementsByClassName("slideshow");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex--;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    if (slideIndex == 0) {
+        slideIndex = 3
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function showSlides() {
+
+    timeOut = 2000;
+
+    var slides = document.getElementsByClassName("slideshow");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
