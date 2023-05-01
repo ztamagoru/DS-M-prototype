a54240149d8aaad42165b83c5cb72d9044a1ec5f
tamagoru <marushannnn@outlook.com>
2023-05-01 02:06:11 -0300

    MEGA DIBUS! MEGA DIBUS EVERYWHERE!

---
+++ js/pedidos.js
@@ -0,0 +1,300 @@
+//#region 
+
+let menu = [
+    {
+        id: "combos",
+        titulo: "Combos",
+        opciones: [
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Doble Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-2.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-3.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-4.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-5.png",
+            },
+        ],
+    },
+    {
+        id: "pollos",
+        titulo: "Pollos y Ensaladas",
+        opciones: [
+            
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+        ],
+    },
+    {
+        id: "snacks",
+        titulo: "Snacks",
+        opciones: [
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+        ],
+    },
+    {
+        id: "cafeteria",
+        titulo: "Cafetería",
+        opciones: [
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+        ],
+    },
+    {
+        id: "postres",
+        titulo: "Postres",
+        opciones: [
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+        ],
+    },
+    {
+        id: "hamburguesas",
+        titulo: "Hamburguesas",
+        opciones: [
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+        ],
+    },
+    {
+        id: "cajita",
+        titulo: "La Cajita de Mostaza",
+        opciones: [
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+        ],
+    },
+    {
+        id: "bebidas",
+        titulo: "Bebidas",
+        opciones: [
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "It was me! Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "https://i.kym-cdn.com/photos/images/newsfeed/000/754/604/d67.jpg",
+            },
+            {
+                nombre: "Combo Mega Dibu Smoked BBQ",
+                descripcion: "Mega hamburguesa de carne con muzarella y barbacoa ahumada, bacon, cebollita crispy, tomate y pan brioche.",
+                precio: "$2400",
+                imagen: "./images/pedidos/1-1.png",
+            },
+        ],
+    }
+];
+
+//#endregion
+
+
+
+// VISTA
+
+function vistaOpcion(opcion) {
+    return document
+    .getElementById("menu-opcion-template")
+    .innerHTML
+    .replace("{{NOMBRE_OPCION}}", opcion.nombre)
+    .replace("{{DESCRIPCION_OPCION}}", opcion.descripcion)
+    .replace("{{PRECIO_OPCION}}", opcion.precio)
+    .replace("{{IMAGEN_OPCION}}", opcion.imagen);
+}
+
+function vistaCategoria(categoria) {
+    let htmlOpciones = categoria.opciones.map(vistaOpcion).join('');
+    
+    let templateCategoria = document.getElementById("menu-categoria-template");
+    return templateCategoria.innerHTML
+    .replace("{{ID_CATEGORIA}}", categoria.id)
+    .replace("{{TITULO_CATEGORIA}}", categoria.titulo)
+    .replace("{{OPCIONES_CATEGORIA}}", htmlOpciones);
+}
+
+function vistaTocItem(categoria) {
+    let templateTocItem = document.getElementById("toc-item-template");
+    return templateTocItem.innerHTML
+        .replace("{{ID_CATEGORIA}}", categoria.id)
+        .replace("{{TITULO_CATEGORIA}}", categoria.titulo);
+}
+
+function loadPage() {
+    let htmlMenu = menu.map(vistaCategoria).join('');
+    let htmlToc = menu.map(vistaTocItem).join('');
+
+    document.getElementById("toc").innerHTML = htmlToc;
+    document.getElementById("menu").innerHTML = htmlMenu;
+}
+
+document.addEventListener('DOMContentLoaded', loadPage)
+
+function toctoc(id_categoria) {
+    // who's there?
+    
+    window.location.hash = id_categoria;
+}
